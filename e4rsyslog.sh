c7fb9b393ede20ffb0ebeb912ade1e79b7dfe333
Nil2a <79719486+Nil2a@users.noreply.github.com>
2021-04-07 19:23:56 +0200

    Update e4rsyslog.sh

--- e4rsyslog.sh
+++ e4rsyslog.sh
@@ -2,6 +2,6 @@
 # E4 RsysLog
 
 #Ajoute ladresse du serveur LibreNMS
-echo "*.* @192.168.60.66:514" >> /etc/rsyslog.conf
+echo "*.4 @192.168.60.66:514" >> /etc/rsyslog.conf
 systemctl restart rsyslog
 systemctl status rsyslog
