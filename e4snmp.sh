bc33b61cacdb4bcf57027cbe9949a84138cc09cd
Nil2a <79719486+Nil2a@users.noreply.github.com>
2021-04-07 18:07:06 +0200

    Update e4snmp.sh

--- e4snmp.sh
+++ e4snmp.sh
@@ -8,7 +8,8 @@ apt install snmp snmpd
 
 # Supprime et télécharge le fichier conf SNMP
 rm /etc/snmp/snmpd.conf
-cd /etc/snmp/ | wget https://raw.githubusercontent.com/Nil2a/Script/main/snmpd.conf
+cd /etc/snmp/
+wget https://raw.githubusercontent.com/Nil2a/Script/main/snmpd.conf
 
 # Restart et affiche le status du service SNMP
 systemctl restart snmpd.service
